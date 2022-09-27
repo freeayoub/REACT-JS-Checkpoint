aeb5fff22caa0580723a83dd99767690a42c9e21
freeayoub <110461494+freeayoub@users.noreply.github.com>
2022-09-27 02:56:34 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Forme from './Forme.js';
+import Navbare from './Navbare.js';
+function App() {
+    
+    return (
+
+    <div className = "App" >
+    
+        <Navbare/>
+        <Forme />
+      
+    
+    </div>
+
+    );
+}
+
+export default App;
